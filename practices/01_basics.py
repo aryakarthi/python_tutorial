19bcc7cec511110a3a990d2742239b043c84e022
aryakarthi <aryaperumal.m@gmail.com>
2024-03-07 14:37:14 +0530

    basics

---
+++ practices/01_basics.py
@@ -0,0 +1,48 @@
+
+
+# # variables
+
+product = "pen"
+product2 = "pencil"
+product_3 = "eraser"
+
+# # input() - to get inputs from users
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print(name + " is " + age + " years old!")
+
+# # type() - returns data type
+
+# city = input("Enter your city: ")
+# print(type(city))
+
+# # to get multiple inputs in singel line from users
+
+# name1, name2, name3 = input("Enter three names : ").split(",")
+# print(name1)
+# print(name2)
+# print(name3)
+
+# # id() - returns memory address
+
+# a = 10
+# b = 20
+# c = a+b
+
+# print(c)
+# print(id(c))
+
+# d = 30
+# print(id(d))
+
+# here, id(c) = id(d) because c=d
+
+
+# # multiline string
+
+my_str = '''
+Hi there!
+Welcome to the python programming!'''
+
+print(my_str)
