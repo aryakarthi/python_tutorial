9d42e8142e4bf08fc0fbfda017134218e6a2c4f3
aryakarthi <aryaperumal.m@gmail.com>
2024-03-07 14:37:30 +0530

    datatypes

---
+++ practices/02_datatypes.py
@@ -0,0 +1,31 @@
+name = "peter"
+print(name)
+print(type(name))
+
+age = 25
+print(age)
+print(type(age))
+
+is_active = True
+print(is_active)
+print(type(is_active))
+
+gpa = 8.2
+print(gpa)
+print(type(gpa))
+
+x = 3+4j
+print(x)
+print(type(x))
+
+fruits = ["apple", "orange", "mango"]
+print(fruits)
+print(type(fruits))
+
+pets = ("dog", "cat", "parrot", "dove")
+print(pets)
+print(type(pets))
+
+directions = {"north", "south", "east", "west"}
+print(directions)
+print(type(directions))
