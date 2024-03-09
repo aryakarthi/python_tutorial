191a3fe00fddcdb96a012ce45db56285798eba4f
aryakarthi <aryaperumal.m@gmail.com>
2024-03-09 21:19:03 +0530

    math functions

---
+++ practices/13_math_functions.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.sqrt(9)) # 3.0
+print(math.floor(6.5673)) # 6
+print(math.ceil(6.5673)) # 7
+print(math.factorial(3)) # 6
+print(math.fabs(-3)) # 3.0
+print(math.pow(5,3)) # 125.0
+print(math.log2(3))
+print(math.log10(3))
+print(math.pi)
+print(math.e)
